079ce124872d69ddab3693de56d9760a41d522a4
aleandr2003 <aleandr2003@gmail.com>
2010-12-20 00:59:46 +0500

    built success

---
+++ VolonteersPortal/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VolonteersPortal.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+
+
+        public ActionResult Received()
+        {
+            return View();
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
+        public ActionResult Read(int Id)
+        {
+            return View();
+        }
+
+        public ActionResult Create(int RecepientId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int RecepientId, string text)
+        {
+            return View();
+        }
+
+        public ActionResult History(int PersonId)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ VolonteersPortal/Controllers/ProjectController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VolonteersPortal.Controllers
+{
+    public class ProjectController : Controller
+    {
+        //
+        // GET: /Project/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Project/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Project/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Project/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Project/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Project/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Apply(int vacancyId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Quit(int vacancyId)
+        {
+            return View();
+        }
+
+        public ActionResult EditName(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditName(int Id, string newName)
+        {
+            return View();
+        }
+
+        public ActionResult EditMotto(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditMotto(int Id, string newMotto)
+        {
+            return View();
+        }
+
+        public ActionResult EditDescription(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditDescription(int Id, string newDescription)
+        {
+            return View();
+        }
+
+        public ActionResult AddVacancy(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddVacancy(int Id, FormCollection collection)
+        {
+            return View();
+        }
+
+        public ActionResult EditVacancy(int Id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditVacancy(int Id, FormCollection collection)
+        {
+            return View();
+        }
+
+        public ActionResult DeleteVacancy(int Id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteVacancy(int Id, FormCollection collection)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult HirePerson(int vacancyId, int personId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DenyPerson(int vacancyId, int personId)
+        {
+            return View();
+        }
+    
+    
+    }
+}
